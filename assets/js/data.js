fafe71a4a72716ccdef629d3a22c92bd1b52c521
kayes360 <42603790+kayes360@users.noreply.github.com>
2025-02-27 22:27:06 +0600

    data,js file added, skills icon reordered

---
+++ assets/js/data.js
@@ -0,0 +1,93 @@
+[
+  {
+    web_app: [
+      {
+        id: 2,
+        projectName: "DrawGeo",
+        description: "This project is a fully functional Next.js application designed to provide polygon creation and management features on an interactive map. It allows users to draw, customize, and manage polygons, calculate their areas, and export/import polygon data. The app integrates React Leaflet for map functionalities and Redux for state management.",
+        technologyUsed: ['react','nextjs','react-leaflet','react-leaflet-draw','SCSS'],
+        liveUrl: "https://drawgeo.vercel.app/",
+        thumbnail: "./thumbnails/drawgro.png",
+      },
+      {
+        id: 3,
+        projectName: "kanban-project-manager",
+        description: "",
+        technologyUsed: [],
+        liveUrl: "",
+        thumbnail: "",
+      },
+      {
+        id: 4,
+        projectName: "URL Manager Browser Extension",
+        description:
+          "URL Manager Browser Extension is perfect for users who want a convenient way to store frequently accessed links, especially job seekers who often save job postings, resources, and important references during job hunts.",
+        technologyUsed: ["React", "TailwindCSS", "Browser Extension"],
+        liveUrl: "https://url-manager-seven.vercel.app/",
+        thumbnail: "./thumbnails/url-manager.png",
+      },
+      {
+        id: 5,
+        projectName: "url-shortener",
+        description:
+          "Url Shortener is a simple and efficient web application that allows users to shorten long URLs. This platform provides users with an easy way to generate, copy, and manage shortened URLs with a user-friendly interface.",
+        technologyUsed: ["React", "Bootstrap", "React Router"],
+        liveUrl: "https://imrul-shorturl.vercel.app/",
+        thumbnail: "./thumbnails/url-shortener.png",
+      },
+      {
+        id: 6,
+        projectName: "image-matcher",
+        description: "",
+        technologyUsed: ["React", "TailwindCSS", "face-api.js"],
+        liveUrl: "",
+        thumbnail: "./thumbnails/image-matcher.jpg",
+      },
+      {
+        id: 7,
+        projectName: "Doctors Appointment Booking Application",
+        description: "A doctors appintment booking application built using react",
+        technologyUsed: ["React", "Vite", "Bootstrap"],
+        liveUrl: "https://ubiquitous-platypus-9dbd44.netlify.app/",
+        thumbnail: "./thumbnails/doctors-appointment-booking-application.png",
+      },
+    ],
+  },
+  {
+    web_ui: [
+      {
+        id: 8,
+        projectName: "NFT Marketplace UI",
+        description:
+          "NFTERS is a digital marketplace for crypto collectibles and non-fungible tokens (NFTs). This platform allows users to buy, sell, and discover exclusive digital assets with a modern and user-friendly interface.",
+        technologyUsed: ["React", "Vite", "Bootstrap"],
+
+        liveUrl: "https://nft-marketplace-ui-gold-seven.vercel.app/",
+        thumbnail: "./thumbnails/nft-marketplace-ui.png",
+      },
+      {
+        id: 9,
+        projectName: "React Bootstrap Landing Page",
+        description: "Simple Landing Page UI developed using react,bootstrap",
+        technologyUsed: ["React", "Vite", "Bootstrap"],
+        liveUrl: "https://react-bootstrap-landing-page-sigma.vercel.app/",
+        thumbnail: "./thumbnails/react-bootstrap-landing-page.png",
+      },
+      {
+        id: 10,
+        projectName: "Collition Box",
+        description:
+          "This project is a React, Vite, and TypeScript-based animated container with moving boxes, rendered using a Canvas element. The animation creates an engaging visual experience",
+        technologyUsed: [
+          "React",
+          "Vite",
+          "TypeScript",
+          "TailwindCSS",
+          "HTML5 Canvas",
+        ],
+        liveUrl: "https://collition-box.vercel.app/",
+        thumbnail: "./thumbnails/collition-Box.png",
+      },
+    ],
+  },
+];
